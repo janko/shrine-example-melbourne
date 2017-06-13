921f6159518c29769d9e1a2324729d0a12f07386
Janko Marohnić <janko.marohnic@gmail.com>
2017-06-13 16:14:30 +1000

    Add attachment field to photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -69,6 +69,6 @@ class PhotosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def photo_params
-      params.require(:photo).permit(:title, :description)
+      params.require(:photo).permit(:title, :description, :image)
     end
 end

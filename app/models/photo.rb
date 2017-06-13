8035fe2fcaa6b4a677a0ec5c1b17c51e59623eca
Janko Marohnić <janko.marohnic@gmail.com>
2017-06-13 16:35:38 +1000

    Add file validations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,5 @@
 class Photo < ApplicationRecord
   include ImageUploader::Attachment.new(:image) # image_data
+
+  validates_presence_of :title, :description, :image
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,6 +7,13 @@ class ImageUploader < Shrine
   plugin :versions
   plugin :determine_mime_type
   plugin :store_dimensions
+  plugin :validation_helpers
+  plugin :cached_attachment_data
+
+  Attacher.validate do
+    validate_mime_type_inclusion %w[image/jpeg image/png], message: "must be an image"
+    validate_max_size 5*1024*1024, message: "must be smaller than 5MB"
+  end
 
   process(:store) do |io|
     original = io.download
